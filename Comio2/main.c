be5270a27852375f820574ba76b1df55bc9df390
Patrice Dietsch <patrice@elsass4.local>
2014-06-23 21:03:14 +0200

    Initial Commit

---
+++ Comio2/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Comio2
+//
+//  Created by Patrice Dietsch on 23/06/2014.
+//  Copyright (c) 2014 Patrice Dietsch. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+   // insert code here...
+   printf("Hello, World!\n");
+    return 0;
+}
+
