855bee511d2b41894bb370914c38b93abae814cf
patrice.dietsch <patrice.dietsch@gmail.com>
2014-07-01 09:39:21 +0000

    Préparation "grille" xplServer

--- src/xplServer.c
+++ src/xplServer.c
@@ -19,12 +19,7 @@
 #include "queue.h"
 #include "memory.h"
 
-#include "interfaces.h"
-#include "interface_type_001.h"
-#include "interface_type_002.h"
-
 #define XPL_VERSION "0.1a2"
-
 
 xPL_ServicePtr xPLService = NULL;
 
@@ -60,45 +55,7 @@ xPL_ServicePtr get_xPL_ServicePtr()
 
 void cmndMsgHandler(xPL_ServicePtr theService, xPL_MessagePtr theMessage, xPL_ObjectPtr userValue)
 {
-   int ret;
-
-   queue_t *interfaces;
-   interfaces_queue_elem_t *iq;
-
-   interfaces=(queue_t *)userValue;
-   
    VERBOSE(9) fprintf(stderr,"%s  (%s) : Reception message xPL\n",INFO_STR,__func__);
-   
-   if(first_queue(interfaces)==-1)
-      return;
-
-   while(1)
-   {
-      current_queue(interfaces, (void **)&iq);
-      switch (iq->type)
-      {
-         case INTERFACE_TYPE_001:
-         {
-            interface_type_001_t *i001 = (interface_type_001_t *)(iq->context);
-            if(i001->xPL_callback)
-               i001->xPL_callback(theService, theMessage, (xPL_ObjectPtr)i001);
-            break;
-         }
-
-         case INTERFACE_TYPE_002:
-         {
-            interface_type_002_t *i002 = (interface_type_002_t *)(iq->context);
-            if(i002->xPL_callback)
-               i002->xPL_callback(theService, theMessage, (xPL_ObjectPtr)i002);
-            break;
-         }
-         default:
-            break;
-      }
-      ret=next_queue(interfaces);
-      if(ret<0)
-         break;
-   }
 }
 
 
@@ -106,17 +63,14 @@ void *_xPL_server_thread(void *data)
 {
    if ( !xPL_initialize(xPL_getParsedConnectionType()) ) return 0 ;
    
-   // myService = xPL_createService(myVendor, myDevice, myInstance);
-   // xPLService = xPL_createService("mnntamoi", hostname, "default");
-   // xPLService = xPL_createService("mea", "edomus", "cheznousdev")
    xPLService = xPL_createService(xpl_vendorID, xpl_deviceID, xpl_instanceID);
    xPL_setServiceVersion(xPLService, XPL_VERSION);
    
    xPL_setRespondingToBroadcasts(xPLService, TRUE);
    
    // xPL_setHeartbeatInterval(xPLService, 5000); // en milliseconde ?
-   
    // xPL_MESSAGE_ANY, xPL_MESSAGE_COMMAND, xPL_MESSAGE_STATUS, xPL_MESSAGE_TRIGGER
+   
    xPL_addServiceListener(xPLService, cmndMsgHandler, xPL_MESSAGE_COMMAND, "control", "basic", (xPL_ObjectPtr)data) ;
    xPL_addServiceListener(xPLService, cmndMsgHandler, xPL_MESSAGE_COMMAND, "sensor", "request", (xPL_ObjectPtr)data) ;
    
@@ -142,7 +96,7 @@ void *_xPL_server_thread(void *data)
 }
 
 
-pthread_t *xPLServer(queue_t *interfaces)
+pthread_t *xPLServer()
 {
    pthread_t *xPL_thread=NULL;
 
@@ -161,7 +115,7 @@ pthread_t *xPLServer(queue_t *interfaces)
       return NULL;
    }
 
-   if(pthread_create (xPL_thread, NULL, _xPL_server_thread, (void *)interfaces))
+   if(pthread_create (xPL_thread, NULL, _xPL_server_thread, NULL)
    {
       VERBOSE(1) fprintf(stderr, "%s (%s) : pthread_create - can't start thread\n",ERROR_STR,__func__);
       return NULL;
