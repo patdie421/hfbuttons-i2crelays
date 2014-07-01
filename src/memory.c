14e3fa1d27d622e347315c50811fc437efdcb822
patrice.dietsch <patrice.dietsch@gmail.com>
2014-07-01 09:41:37 +0000

    Pour xplServer

---
+++ src/memory.c
@@ -0,0 +1,54 @@
+//
+//  memory.c
+//
+//  Created by Patrice DIETSCH on 17/09/12.
+//
+//
+
+#include "debug.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+char *string_malloc_and_copy(char *str, int v)
+{
+   char *new_str=(char *)malloc(strlen(str)+1);
+   if(new_str)
+      strcpy(new_str,str);
+   else
+   {
+      VERBOSE(1) {
+         fprintf (stderr, "%s (%s) : %s - ",ERROR_STR,__func__,MALLOC_ERROR_STR);
+         perror("");
+      }
+      return NULL;
+   }
+   return new_str;
+}
+
+
+char *string_free_malloc_and_copy(char **org_str, char *str, int v)
+{
+   if(*org_str)
+   {
+      free(*org_str);
+      *org_str=NULL;
+   }
+   
+   *org_str=(char *)malloc(strlen(str)+1);
+   if(*org_str)
+      strcpy(*org_str,str);
+   else
+   {
+      VERBOSE(1) {
+         fprintf (stderr, "%s (%s) : %s - ",ERROR_STR,__func__,MALLOC_ERROR_STR);
+         perror("");
+      }
+      return NULL;
+   }
+   return *org_str;
+}
+
+
