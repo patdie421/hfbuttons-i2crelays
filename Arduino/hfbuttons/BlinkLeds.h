153d1b812e65575c5486c5d72a0f065736cd2dd4
patrice dietsch <patrice.dietsch@gmail.com>
2014-07-08 08:45:10 +0200

    ajout des fichiers de hfbuttons

---
+++ Arduino/hfbuttons/BlinkLeds.h
@@ -0,0 +1,18 @@
+/* pour un clignotement de led synchronisé */
+#ifndef __BLINKLEDS__
+#define __BLINKLEDS__
+class BlinkLeds
+{
+public:
+  BlinkLeds(unsigned int i);
+  void run();
+  int getLedState() {
+    return ledState;
+  }
+  void blink(int pin);
+private:
+  unsigned int interval;
+  int ledState;
+  unsigned long previousMillis;
+};
+#endif
